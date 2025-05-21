af54b1c54993da11a278df6cd6acc54f8b5a3bfd
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-05-21 10:36:00 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { take } from 'rxjs';
@@ -46,9 +44,7 @@ export interface ImperativConjugation {
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, FormsModule],
-  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
