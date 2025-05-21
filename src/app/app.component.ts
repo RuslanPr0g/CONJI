2d70c74e02c0e0fae23b3e034416359f5048129d
Ruslan Rudenko <theruslan.prog@gmail.com>
2025-05-21 10:16:15 +0200

    add popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,83 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { take } from 'rxjs';
+
+type ConjugationKey = keyof Conjugations;
+
+export interface VerbGroup {
+  group: string;
+  verbs: Verb[];
+}
+
+export interface Verb {
+  infinitive: string;
+  conjugations: Conjugations;
+}
+
+export interface Conjugations {
+  prezent: ConjugationSet;
+  perfect_compus: ConjugationSet;
+  viitor_literar: ConjugationSet;
+  viitor_familiar: ConjugationSet;
+  conjunctiv: ConjugationSet;
+  conditional: ConjugationSet;
+  imperativ: ImperativConjugation;
+}
+
+export interface ConjugationSet {
+  eu: string;
+  tu: string;
+  'el/ea': string;
+  noi: string;
+  voi: string;
+  'ei/ele': string;
+}
+
+export interface ImperativConjugation {
+  tu: string;
+  voi: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'conji';
+  groupedVerbs: VerbGroup[] = [];
+  searchText: string = '';
+  title: string = 'CONJI APP';
+  selectedVerb: Verb | null = null;
+  conjugationKeys: ConjugationKey[] = [
+    'prezent',
+    'perfect_compus',
+    'viitor_literar',
+    'viitor_familiar',
+    'conjunctiv',
+    'conditional',
+    'imperativ',
+  ];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<VerbGroup[]>('verbs.json')
+      .pipe(take(1))
+      .subscribe((data) => {
+        this.groupedVerbs = data;
+      });
+  }
+
+  openPopup(verb: Verb) {
+    this.selectedVerb = verb;
+  }
+
+  closePopup() {
+    this.selectedVerb = null;
+  }
 }
