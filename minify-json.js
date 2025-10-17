aa2a2139092be27223832c78acf13058d39ccbc4
RuslanPr0g <theruslan.prog@gmail.com>
2025-10-17 06:01:33 +0200

    bugfixes

--- minify-json.js
+++ minify-json.js
@@ -1,27 +1,27 @@
-const fs = require("fs");
-const path = require("path");
-const jsonminify = require("jsonminify");
+const fs = require('fs');
+const path = require('path');
+const jsonminify = require('jsonminify');
 
-const folderPath = path.resolve(__dirname, "public");
+const folderPath = path.resolve(__dirname, 'apps/conji/public');
 
 const allFiles = fs.readdirSync(folderPath);
 for (const file of allFiles) {
-  if (file.endsWith(".min.json")) {
+  if (file.endsWith('.min.json')) {
     fs.unlinkSync(path.join(folderPath, file));
   }
 }
 
 const files = allFiles.filter(
-  (f) => f.endsWith(".json") && !f.endsWith(".min.json")
+  (f) => f.endsWith('.json') && !f.endsWith('.min.json')
 );
 
 for (const file of files) {
   const filePath = path.join(folderPath, file);
-  const content = fs.readFileSync(filePath, "utf-8");
+  const content = fs.readFileSync(filePath, 'utf-8');
   try {
     const minified = jsonminify(content);
-    const minFilePath = filePath.replace(/\.json$/, ".min.json");
-    fs.writeFileSync(minFilePath, minified, "utf-8");
+    const minFilePath = filePath.replace(/\.json$/, '.min.json');
+    fs.writeFileSync(minFilePath, minified, 'utf-8');
     console.log(`Minified ${file}`);
   } catch (e) {
     console.error(`Failed to minify ${file}:`, e);
